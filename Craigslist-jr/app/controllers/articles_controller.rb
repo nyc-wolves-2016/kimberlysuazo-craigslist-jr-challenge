bbe82be6fb5ddd1a8b28a0e075700749ef4c7b49
Kimberly Suazo <email": null>
2016-10-03 16:06:39 -0400

    Add show page for Article. Add link to connect to it from Category page

--- Craigslist-jr/app/controllers/articles_controller.rb
+++ Craigslist-jr/app/controllers/articles_controller.rb
@@ -1,2 +1,22 @@
 class ArticlesController < ApplicationController
+
+  def create
+    @category = Category.find_by(id: params[:category_id])
+    @article = @category.articles.new(article_params)
+    @article.key = SecureRandom.hex(4)
+    if @article.save
+      redirect_to category_path(@category)
+    else
+      render 'categories/show'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
