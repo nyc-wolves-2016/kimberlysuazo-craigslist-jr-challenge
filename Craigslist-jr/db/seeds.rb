d54167fe8a27b80e6cda1a806910ba8713b93e63
Kimberly Suazo <email": null>
2016-10-03 17:44:35 -0400

    Add seeds

--- Craigslist-jr/db/seeds.rb
+++ Craigslist-jr/db/seeds.rb
@@ -3,3 +3,8 @@
 
 c = Category.create!(name: "Furniture for Sale")
 a = Article.create!(title:"Red sofa", content: "Moving out of town and need to get rid of this very comfy sofa. Pet/Smoke/Bed-bug -free home. $250", category_id: c.id, key: SecureRandom.hex(4))
+b= Article.create!(title:"Dining Table", content: "Square wood table. Seats 4 comfortably. Minor scratches $300.", category_id: c.id, key: SecureRandom.hex(4))
+d = Article.create!(title:"Round Glass Coffee Table", content: "glass top. from ikea $150.", category_id: c.id, key: SecureRandom.hex(4))
+
+c = Category.create!(name: "Apartments for Rent")
+a = Article.create!(title:"1BR in Chelsea, $1800/month", content: "Two elevators in building. 1 BR, 1 Bath, Open kitchen and dining area.", category_id: c.id, key: SecureRandom.hex(4))
