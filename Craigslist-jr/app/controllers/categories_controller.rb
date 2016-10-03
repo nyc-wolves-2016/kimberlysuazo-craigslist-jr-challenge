793f4750929f770e9722d825acbbfb54022fab84
Kimberly Suazo <email": null>
2016-10-03 12:32:43 -0400

    Add new/create for category controller. Add associations to classes

--- Craigslist-jr/app/controllers/categories_controller.rb
+++ Craigslist-jr/app/controllers/categories_controller.rb
@@ -1,5 +1,19 @@
 class CategoriesController < ApplicationController
+
   def index
     @categories = Category.all
   end
+
+  def new
+  end
+
+  def create
+    @category = Category.new(category_params)
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
