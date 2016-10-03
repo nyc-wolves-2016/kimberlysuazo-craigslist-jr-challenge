5643fb928cda89295fcd9e8d28cab4d0f0915eb6
Kimberly Suazo <email": null>
2016-10-03 15:13:37 -0400

    Move new category form to partial

--- Craigslist-jr/db/seeds.rb
+++ Craigslist-jr/db/seeds.rb
@@ -1,9 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 c = Category.create!(name: "Furniture for Sale")
 a = Article.create!(title:"Red sofa", content: "Moving out of town and need to get rid of this very comfy sofa. Pet/Smoke/Bed-bug -free home. $250", category_id: c.id, key: SecureRandom.hex(4))
